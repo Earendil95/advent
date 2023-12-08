eb644a7e5de220d2a06c382abbe441a63e5d6636
Earendil95 <ermak95@gmail.com>
2023-12-08 16:59:06 +0100

    day 8

--- 2023/day_08/2.rb
+++ 2023/day_08/2.rb
@@ -30,7 +30,7 @@ class Node
   end
 end
 
-map = File.readlines(ARGV[0] || 'test_input.txt', chomp: true)
+map = File.readlines(ARGV[0] || 'test_input_3.txt', chomp: true)
 
 instructions = map[0]
 nodes = map[2..]
@@ -38,15 +38,22 @@ nodes_network = NodesNetwork.new
 
 nodes.map! { nodes_network.add_node(Node.new(_1, nodes_network)) }
 
-current_node = nodes_network.fetch('AAA')
-steps_count = 0
-instruction_number = 0
+current_nodes = nodes.select { _1.label.end_with?('A') }
+steps_counts = []
 
-until current_node.label == 'ZZZ'
-  direction = instructions[instruction_number]
-  current_node = current_node.go_to(direction)
-  steps_count += 1
-  instruction_number = (instruction_number + 1) % instructions.size
+current_nodes.each do |node|
+  current_node = node
+  steps_count = 0
+  instruction_number = 0
+
+  until current_node.label.end_with?('Z')
+    direction = instructions[instruction_number]
+    current_node = current_node.go_to(direction)
+    steps_count += 1
+    instruction_number = (instruction_number + 1) % instructions.size
+  end
+
+  steps_counts << steps_count
 end
 
-puts steps_count
+puts steps_counts.inject(1) { |a, e| a.lcm(e) }
